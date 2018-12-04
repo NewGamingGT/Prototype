e74e9a66bdaaf292062d05cdb77e6047d9f3ed7a
NewGaming <39025657+NewGamingGT@users.noreply.github.com>
2018-12-04 21:53:39 +0800

    Add walls

---
+++ SimpleAI/Assets/Ai.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ai : MonoBehaviour {
+
+    NavMeshAgent navMeshAgent;
+
+    Transform TargetPos;
+    GameObject targetPos;
+    Vector3 Target;
+
+	// Use this for initialization
+	void Start () {
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+
+        targetPos = GameObject.Find("Target");
+        TargetPos = targetPos.transform; 
+        //Finds the game object then get the transform position and store it
+
+        Target.z = TargetPos.position.z;
+        Target.x = TargetPos.position.x;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        navMeshAgent.SetDestination(Target);
+	}
+}
---
+++ SimpleAI/Assets/PlaceBlocks.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlaceBlocks : MonoBehaviour {
+
+    public TextMeshProUGUI BlocksPlacedUI;
+    //UI
+
+    public Camera cam;
+
+    public GameObject blocks;
+    public Vector3 hitPos;
+
+    public LayerMask mask;
+
+    bool canPlace = true;
+
+    public int CurrentBlockPlaced = 0;
+    public int MaxBlockPlaced = 20;
+
+    public float grid = 2.5f;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetMouseButton(0))
+        {
+            if(Physics.Raycast(ray , out hit))
+            {
+                hitPos.x = Mathf.Round(hit.point.x);
+                hitPos.z = Mathf.Round(hit.point.z); 
+
+                if (hitPos.y < 0.1 && hit.collider.gameObject.tag != "Troops" && hit.collider.gameObject.tag != "Obstacle" && CurrentBlockPlaced <   MaxBlockPlaced)
+                {
+                    Instantiate(blocks, hitPos, Quaternion.identity);
+                    CurrentBlockPlaced++;
+                    BlocksPlacedUI.text = CurrentBlockPlaced + " / " + MaxBlockPlaced + " blocks placed";
+                }
+            }
+        }
+
+
+	}
+
+}
